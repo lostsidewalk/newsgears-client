c873343166e1c89560a286ffdd6693b9b0158be2
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2023-05-19 06:45:17 -0500

    ESLint (password-update-panel)

--- src/components/password-update-panel/PasswordUpdatePanel.vue
+++ src/components/password-update-panel/PasswordUpdatePanel.vue
@@ -1,12 +1,23 @@
 <template>
   <v-container>
     <!-- row -->
-    <v-row align="center" justify="center">
+    <v-row
+      align="center"
+      justify="center"
+    >
       <!-- col -->
-      <v-col cols="12" sm="10">
+      <v-col
+        cols="12"
+        sm="10"
+      >
         <v-hover v-slot="{ isHovering, props }">
           <!-- card -->
-          <v-card :elevation="isHovering ? 7 : 6" class="mt-10" v-bind="props" variant="outlined">
+          <v-card
+            :elevation="isHovering ? 7 : 6"
+            class="mt-10"
+            v-bind="props"
+            variant="outlined"
+          >
             <!-- window -->
             <v-window v-model="step">
               <!-- window item -->
@@ -18,45 +29,67 @@
                     <!-- card-text -->
                     <v-card-text class="mt-12 mb-12">
                       <!-- banner (large) -->
-                      <h4 class="text-center mt-4 mb-4">{{ this.$t('passwordReset') }}</h4>
+                      <h4 class="text-center mt-4 mb-4">
+                        {{ $t('passwordReset') }}
+                      </h4>
                       <!-- banner (small) -->
-                      <h6 class="text-center mt-4-mb-4">{{ this.$t('enterAndConfirmNewPw') }}</h6>
+                      <h6 class="text-center mt-4-mb-4">
+                        {{ $t('enterAndConfirmNewPw') }}
+                      </h6>
                       <!-- row -->
-                      <v-row align="center" justify="center">
+                      <v-row
+                        align="center"
+                        justify="center"
+                      >
                         <!-- col -->
-                        <v-col cols="12" sm="8">
+                        <v-col
+                          cols="12"
+                          sm="8"
+                        >
                           <!-- new password -->
-                          <AuthTextField class="mt-4 mb-4"
+                          <AuthTextField
+                            class="mt-4 mb-4"
                             type="password"
-                            :label="this.$t('newPassword')" 
-                            :placeholder="this.$t('newPassword')" 
-                            :theme="theme" 
-                            :modelValue="this.newPassword" 
-                            @update:modelValue="this.newPassword = $event" 
-                            />
+                            :label="$t('newPassword')" 
+                            :placeholder="$t('newPassword')" 
+                            :model-value="newPassword" 
+                            @update:modelValue="newPassword = $event" 
+                          />
                           <!-- new password (confirm) -->
-                          <AuthTextField class="mt-4 mb-4"
+                          <AuthTextField
+                            class="mt-4 mb-4"
                             type="password"
-                            :label="this.$t('confirmNewPassword')" 
-                            :placeholder="this.$t('confirmNewPassword')" 
-                            :theme="theme" 
-                            :modelValue="this.newPasswordConfirmed" 
-                            @update:modelValue="this.newPasswordConfirmed = $event" 
-                            />
+                            :label="$t('confirmNewPassword')" 
+                            :placeholder="$t('confirmNewPassword')" 
+                            :model-value="newPasswordConfirmed" 
+                            @update:modelValue="newPasswordConfirmed = $event" 
+                          />
                           <div class="d-flex flex-row flex-wrap">
                             <!-- submit button -->
                             <AuthButton
-                              :label="this.$t('submit')"
-                              :theme="theme"
+                              :label="$t('submit')"
+                              :in-transit="pwUpdateInTransit"
                               @clicked="submitPwUpdate()"
-                              :inTransit="pwUpdateInTransit" />
+                            />
                           </div>
-                          <AuthServerResponse :serverMessage="serverMessage" :theme="theme" />
-                          <v-btn variant="text" class="mt-4 mb-4 pa-2" block 
-                            @click="this.showPrivacyPolicy = !this.showPrivacyPolicy" 
-                            :text="this.$t('privacyPolicy')" />
-                          <v-dialog v-model="this.showPrivacyPolicy" width="75%" height="75%" scrollable>
-                            <PrivacyPolicyPanel @dismiss="this.showPrivacyPolicy = false" />
+                          <AuthServerResponse
+                            :server-message="serverMessage"
+                            :theme="theme"
+                          />
+                          <v-btn
+                            variant="text"
+                            class="mt-4 mb-4 pa-2"
+                            block 
+                            :text="$t('privacyPolicy')" 
+                            @click="showPrivacyPolicy = !showPrivacyPolicy"
+                          />
+                          <v-dialog
+                            v-model="showPrivacyPolicy"
+                            width="75%"
+                            height="75%"
+                            scrollable
+                          >
+                            <PrivacyPolicyPanel @dismiss="showPrivacyPolicy = false" />
                           </v-dialog>
                         </v-col>
                       </v-row>  
@@ -85,8 +118,7 @@ export default {
     AuthButton,
     AuthServerResponse, 
     PrivacyPolicyPanel, 
-},
-  props: [ "theme" ],
+  },
   data() {
     return {
       step: null,
